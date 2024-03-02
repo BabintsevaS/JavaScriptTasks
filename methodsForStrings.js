2d3d726f20d9f575a9d37d9648d9f3805d2def6d
BabintsevaS <sb.babintseva@gmail.com>
2024-03-01 21:27:17 -0600

    2nd commit

--- methodsForStrings.js
+++
@@ -1,7 +0,0 @@
-let text1 = "Hello World!";
-let text2 = text1.toUpperCase();
-console.log(text2);
-
-let text = "The rain in SPAIN stays mainly in the plain";
-const myArr = text.match("ain");
-console.log(myArr);
